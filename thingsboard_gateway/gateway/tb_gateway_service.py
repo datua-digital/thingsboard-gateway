711b66489249dc83612da4d9fac9e4067aee53ec
gudari <aartola1986@gmail.com>
2024-08-19 15:57:01 +0200

    use other connect

--- thingsboard_gateway/gateway/tb_gateway_service.py
+++ thingsboard_gateway/gateway/tb_gateway_service.py
@@ -537,7 +537,7 @@ class TBGatewayService:
                 else:
                     sleep(.2)
                     log.debug("Thingsboard client is not connected.")
-                    self.tb_client.connect()
+                    self.tb_client.client.connect()
             except Exception as e:
                 log.exception(e)
                 sleep(1)
